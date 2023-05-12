93fa71de1e8abce4da0f67767ebb18d4e9a562ff
Harishankar Kumar <hari01584@gmail.com>
2023-05-12 06:05:46 +0530

    GRAPHICS: implement macpopupmenu & move structs to header
    macpopup is implemented and other modifications made to allow macmenu to be extended

---
+++ graphics/macgui/macpopupmenu.cpp
@@ -0,0 +1,80 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "graphics/macgui/macpopupmenu.h"
+
+namespace Graphics {
+
+MacPopUp::MacPopUp(int id, const Common::Rect &bounds, MacWindowManager *wm, const char *string) : MacMenu(id, bounds, wm) {
+	_menuItemId = addMenuItem(nullptr, "");
+	createSubMenuFromString(0, string, 0);
+	wm->addMenu(id, this);
+	_menuId = id;
+}
+
+bool MacPopUp::draw(ManagedSurface *g, bool forceRedraw) {
+	Common::Rect r(_bbox);
+
+	if (!_isVisible)
+		return false;
+
+	if (_dimensionsDirty)
+		calcDimensions(_mouseX, _mouseY);
+
+	if (!_contentIsDirty && !forceRedraw)
+		return false;
+	_contentIsDirty = false;
+
+	_screen.clear(_wm->_colorGreen);
+	renderSubmenu(_items[0]->submenu, true);
+
+	if (g)
+		g->transBlitFrom(_screen, _wm->_colorGreen);
+
+	if (!(_wm->_mode & kWMModalMenuMode) && g)
+		g_system->copyRectToScreen(g->getPixels(), g->pitch, 0, 0, g->w, g->h);
+
+	return true;
+}
+
+uint32 MacPopUp::drawAndSelectMenu(int x, int y, int item) {
+	_mouseX = x;
+	_mouseY = y;
+
+	// Open selector first
+	mouseClick(x, y, true);
+
+	int activeSubItem = getLastSelectedSubmenuItem();
+	if (activeSubItem == -1)
+		return item;
+
+	// Return one indexed item!
+	return activeSubItem + 1;
+}
+
+Common::String MacPopUp::getItemText(int item) {
+	// Convert 1-indexed item to 0 indexed
+	item = item - 1;
+	MacMenuItem *menu = getMenuItem(_menuItemId);
+	MacMenuItem *submenu = getSubMenuItem(menu, item);
+	return getName(submenu);
+}
+} // end of namespace Graphics
